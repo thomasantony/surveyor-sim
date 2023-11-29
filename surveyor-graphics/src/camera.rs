972c40be5a39d915c7c7bc1ca3c4ffffc6d3dd78
Thomas Antony <mail@thomasantony.com>
2023-11-28 19:20:17 -0800

    clean up camera.rs

--- surveyor-graphics/src/camera.rs
+++ surveyor-graphics/src/camera.rs
@@ -52,56 +52,6 @@ pub fn spawn_camera(
     println!("camera Spawned");
 }
 
-// pub fn camera_input_map(
-//     mut events: EventWriter<ControlEvent>,
-//     mut mouse_wheel_reader: EventReader<MouseWheel>,
-//     mut mouse_motion_events: EventReader<MouseMotion>,
-//     mouse_buttons: Res<Input<MouseButton>>,
-//     keyboard: Res<Input<KeyCode>>,
-//     controllers: Query<&OrbitCameraController>,
-// ) {
-//     // Can only control one camera at a time.
-//     let controller = if let Some(controller) = controllers.iter().find(|c| c.enabled) {
-//         controller
-//     } else {
-//         return;
-//     };
-//     let OrbitCameraController {
-//         mouse_rotate_sensitivity,
-//         mouse_translate_sensitivity,
-//         mouse_wheel_zoom_sensitivity,
-//         pixels_per_line,
-//         ..
-//     } = *controller;
-
-//     let mut cursor_delta = Vec2::ZERO;
-//     for event in mouse_motion_events.iter() {
-//         cursor_delta += event.delta;
-//     }
-
-//     // if keyboard.pressed(KeyCode::LControl) {
-//     if mouse_buttons.pressed(MouseButton::Left) {
-//         events.send(ControlEvent::Orbit(mouse_rotate_sensitivity * cursor_delta));
-//     }
-
-//     if mouse_buttons.pressed(MouseButton::Right) {
-//         events.send(ControlEvent::TranslateTarget(
-//             mouse_translate_sensitivity * cursor_delta,
-//         ));
-//     }
-
-//     let mut scalar = 1.0;
-//     for event in mouse_wheel_reader.iter() {
-//         // scale the event magnitude per pixel or per line
-//         let scroll_amount = match event.unit {
-//             MouseScrollUnit::Line => event.y,
-//             MouseScrollUnit::Pixel => event.y / pixels_per_line,
-//         };
-//         scalar *= 1.0 - scroll_amount * mouse_wheel_zoom_sensitivity;
-//     }
-//     events.send(ControlEvent::Zoom(scalar));
-// }
-
 pub fn sync_camera(
     phy_query: Query<&OrbitalDynamics>,
     mut camera_query: Query<(&mut Transform, &mut GridCell<GridCellType>, &CameraRelativePosition, &mut PanOrbitCamera), With<Camera>>,
